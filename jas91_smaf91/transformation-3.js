e45eff829d87af96c6bb887c7eb9b5daa37cf6b5
Stephanie Alibrandi <smaf91@bu.edu>
2016-09-28 14:34:55 -0400

    Boston University GRS CS 591
    Added third transformation draft
    
    	Add:
            transformation-3.js

---
+++ jas91_smaf91/transformation-3.js
@@ -0,0 +1,58 @@
+/*
+ * Transformation 3
+ *
+ * Description:
+ *       Finds the number of crimes and 311 reports in a 100ft sqare area. Based
+ *       on latitude and longitude.
+ */
+
+function project(x) {
+    return {geo_info: x.geo_info}
+}
+
+function union(X, Y, XY, f) {
+    //db[XY].remove({});
+   // db.createCollection(XY);
+    db[X].find().forEach(function(x) {
+        db[XY].insert(f(x));
+    });
+    db[Y].find().forEach(function(y) {
+        db[XY].insert(f(y));
+    });
+}
+
+db['jas91_smaf91.union_temp'].remove({});
+db.createCollection('jas91_smaf91.union_temp');
+union('jas91_smaf91.sr311', 'jas91_smaf91.food', 'jas91_smaf91.union_temp', project);
+union('jas91_smaf91.schools', 'jas91_smaf91.hospitals', 'jas91_smaf91.union_temp', project);
+
+db.jas91_smaf91.union_temp.mapReduce(
+    function(){
+        var id = {
+            latitude:  this.geo_info.geometry.coordinates[0], 
+            longitude: this.geo_info.geometry.coordinates[1],
+        }
+        emit(id, {zip_code:this.geo_info.properties.zip_code});
+    },
+    function(k,vs) {
+        return vs[0] 
+    },
+    { 
+        out: "jas91_smaf91.coordinates_zip_codes"
+    }
+);
+
+db.jas91_smaf91.crime.find().forEach(
+        function (x) {
+            var id = {
+                latitude:  x.geo_info.geometry.coordinates[0], 
+                longitude: x.geo_info.geometry.coordinates[1]
+            };
+            match = db.jas91_smaf91.coordinates_zip_codes.findOne({_id: id});
+            if (match) {
+                zip_code = match.value.zip_code; 
+                x.geo_info.properties.zip_code = zip_code;
+                db.jas91_smaf91.crime.save(x);
+            }
+        }
+);
