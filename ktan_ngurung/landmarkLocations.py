5c3fcf2ac01fd5c7240d05097fc8551cf2938678
Kristel Tan <ktan@bu.edu>
2016-09-28 16:31:53 -0400

    modified alice_bob folder

--- ktan_ngurung/landmarkLocations.py
+++ ktan_ngurung/landmarkLocations.py
@@ -4,6 +4,7 @@ import dml
 import prov.model
 import datetime
 import uuid
+import os
 
 class example(dml.Algorithm):
     contributor = 'ktan_ngurung'
@@ -15,45 +16,50 @@ class example(dml.Algorithm):
         '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
         startTime = datetime.datetime.now()
         
+        current_dir = os.getcwd()
+        parent = os.path.normpath(os.path.join(current_dir, ".."))
+
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('ktan_ngurung', 'ktan_ngurung')
 
-        filename = 'json-data/big-belly-locations.json'
-        res = open(filen, 'r')
-        r0 = json.load(res)
+        url = 'https://raw.githubusercontent.com/ktango/course-2016-fal-proj/master/json-data/big-belly-locations.json'
+        response = urllib.request.urlopen(url).read().decode('utf-8')
+        r0 = json.loads(response)
+        s0 = json.dumps(r0, sort_keys=True, indent=2)
         repo.dropPermanent("bigBelly")
         repo.createPermanent("bigBelly")
-        repo['ktan_ngurung.bigBelly'].insert_many(r0)
+        repo['ktan_ngurung.bigBelly'].insert_one(r0)
 
-        filename = 'json-data/colleges-and-universities.json'
-        res = open(filen, 'r')
-        r1 = json.load(res)
+        url = 'https://raw.githubusercontent.com/ktango/course-2016-fal-proj/master/json-data/colleges-and-universities.json'
+        response = urllib.request.urlopen(url).read().decode('utf-8')
+        r1 = json.load(response)
+        s1 = json.dumps(r1, sort_keys=True, indent=2)
         repo.dropPermanent("colleges")
         repo.createPermanent("colleges")
         repo['ktan_ngurung.colleges'].insert_many(r1)
 
-        filename = 'json-data/hubway-stations-in-boston.json'
-        res = open(filen, 'r')
-        r2 = json.load(res)
-        repo.dropPermanent("hubways")
-        repo.createPermanent("hubways")
-        repo['ktan_ngurung.hubways'].insert_many(r2)
+        # filename = parent + '/json-data/hubway-stations-in-boston.json'
+        # res = open(filename, 'r')
+        # r2 = json.load(res)
+        # repo.dropPermanent("hubways")
+        # repo.createPermanent("hubways")
+        # repo['ktan_ngurung.hubways'].insert_many(r2)
 
-        filename = 'mbta-bus-stops.json'
-        res = open(filen, 'r')
-        r3 = json.load(res)
-        repo.dropPermanent("busStops")
-        repo.createPermanent("busStops")
-        repo['ktan_ngurung.busStops'].insert_many(r3)
+        # filename = parent + '/json-data/mbta-bus-stops.json'
+        # res = open(filename, 'r')
+        # r3 = json.load(res)
+        # repo.dropPermanent("busStops")
+        # repo.createPermanent("busStops")
+        # repo['ktan_ngurung.busStops'].insert_many(r3)
 
-        filename = 't-stop-locations.json'
-        res = open(filen, 'r')
-        r4 = json.load(res)
-        repo.dropPermanent("tStops")
-        repo.createPermanent("tStops")
-        repo['ktan_ngurung.tStops'].insert_many(r4)
+        # filename = parent + '/json-data/t-stop-locations.json'
+        # res = open(filename, 'r')
+        # r4 = json.load(res)
+        # repo.dropPermanent("tStops")
+        # repo.createPermanent("tStops")
+        # repo['ktan_ngurung.tStops'].insert_many(r4)
 
         repo.logout()
 
@@ -61,56 +67,58 @@ class example(dml.Algorithm):
 
         return {"start":startTime, "end":endTime}
 
-#     @staticmethod
-#     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
-#         '''
-#         Create the provenance document describing everything happening
-#         in this script. Each run of the script will generate a new
-#         document describing that invocation event.
-#         '''
+    @staticmethod
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        '''
+        Create the provenance document describing everything happening
+        in this script. Each run of the script will generate a new
+        document describing that invocation event.
+        '''
 
-#          # Set up the database connection.
-#         client = dml.pymongo.MongoClient()
-#         repo = client.repo
-#         repo.authenticate('ktan_ngurung', 'ktan_ngurung')
+         # Set up the database connection.
+        # client = dml.pymongo.MongoClient()
+        # repo = client.repo
+        # repo.authenticate('ktan_ngurung', 'ktan_ngurung')
 
-#         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
-#         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
-#         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
-#         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-#         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+        # doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filenameame> format.
+        # doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        # doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        # doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        # doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-#         this_script = doc.agent('alg:ktan_ngurung#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-#         resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-#         get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-#         get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-#         doc.wasAssociatedWith(get_found, this_script)
-#         doc.wasAssociatedWith(get_lost, this_script)
-#         doc.usage(get_found, resource, startTime, None,
-#                 {prov.model.PROV_TYPE:'ont:Retrieval',
-#                  'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
-#                 }
-#             )
-#         doc.usage(get_lost, resource, startTime, None,
-#                 {prov.model.PROV_TYPE:'ont:Retrieval',
-#                  'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
-#                 }
-#             )
+        # this_script = doc.agent('alg:ktan_ngurung#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        # resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        # get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        # get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        # doc.wasAssociatedWith(get_found, this_script)
+        # doc.wasAssociatedWith(get_lost, this_script)
+        # doc.usage(get_found, resource, startTime, None,
+        #         {prov.model.PROV_TYPE:'ont:Retrieval',
+        #          'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+        #         }
+        #     )
+        # doc.usage(get_lost, resource, startTime, None,
+        #         {prov.model.PROV_TYPE:'ont:Retrieval',
+        #          'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
+        #         }
+        #     )
 
-#         lost = doc.entity('dat:ktan_ngurung#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
-#         doc.wasAttributedTo(lost, this_script)
-#         doc.wasGeneratedBy(lost, get_lost, endTime)
-#         doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+        # lost = doc.entity('dat:ktan_ngurung#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
+        # doc.wasAttributedTo(lost, this_script)
+        # doc.wasGeneratedBy(lost, get_lost, endTime)
+        # doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
 
-#         found = doc.entity('dat:ktan_ngurung#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
-#         doc.wasAttributedTo(found, this_script)
-#         doc.wasGeneratedBy(found, get_found, endTime)
-#         doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+        # found = doc.entity('dat:ktan_ngurung#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
+        # doc.wasAttributedTo(found, this_script)
+        # doc.wasGeneratedBy(found, get_found, endTime)
+        # doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
 
-#         repo.record(doc.serialize()) # Record the provenance document.
-#         repo.logout()
+        # repo.record(doc.serialize()) # Record the provenance document.
+        # repo.logout()
 
-#         return doc
+        # return doc
+        pass
+
 
 example.execute()
 # doc = example.provenance()
