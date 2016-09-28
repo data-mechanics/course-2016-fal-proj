ea5c9cc5a93953bad41c1f671aa82b7ddc7609cd
hyh3898 <huangyh@bu.edu>
2016-09-27 23:03:37 -0400

    Get the data of hotpitals Json

--- aydenbu_huangyh/example.py
+++ aydenbu_huangyh/example.py
@@ -6,9 +6,9 @@ import datetime
 import uuid
 
 class example(dml.Algorithm):
-    contributor = 'alice_bob'
+    contributor = 'aydenbu_huangyh'
     reads = []
-    writes = ['alice_bob.lost', 'alice_bob.found']
+    writes = ['aydenbu_huangyh.lost', 'aydenbu_huangyh.found']
 
     @staticmethod
     def execute(trial = False):
@@ -18,23 +18,47 @@ class example(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
+        repo.authenticate('aydenbu_huangyh', 'aydenbu_huangyh')
 
-        url = 'http://cs-people.bu.edu/lapets/591/examples/lost.json'
+        # url = 'http://cs-people.bu.edu/lapets/591/examples/lost.json'
+        # response = urllib.request.urlopen(url).read().decode("utf-8")
+        # r = json.loads(response)
+        # s = json.dumps(r, sort_keys=True, indent=2)
+        # repo.dropPermanent("lost")
+        # repo.createPermanent("lost")
+        # repo['aydenbu_huangyh.lost'].insert_many(r)
+        #
+        # url = 'http://cs-people.bu.edu/lapets/591/examples/found.json'
+        # response = urllib.request.urlopen(url).read().decode("utf-8")
+        # r = json.loads(response)
+        # s = json.dumps(r, sort_keys=True, indent=2)
+        # repo.dropPermanent("found")
+        # repo.createPermanent("found")
+        # repo['aydenbu_huangyh.found'].insert_many(r)
+        #
+        # url = "https://data.cityofboston.gov/resource/jsri-cpsq.json"
+        # response = urllib.request.urlopen(url).read().decode("utf-8")
+        # r = json.loads(response)
+        # s = json.dumps(r, sort_keys=True, indent=2)
+        # repo.dropPermanent("jsri-cpsq")
+        # repo.createPermanent("jsri-cpsq")
+        # repo['aydenbu_huangyh.jsri-cpsq'].insert_many(r)
+
+        # url = "https://data.cityofboston.gov/resource/u6fv-m8v4.json"
+        # response = urllib.request.urlopen(url).read().decode("utf-8")
+        # r = json.loads(response)
+        # s = json.dumps(r, sort_keys=True, indent=2)
+        # repo.dropPermanent("hospitalLoc")
+        # repo.createPermanent("hospitalLoc")
+        # repo['aydenbu_huangyh.hospitalLoc'].insert_many(r)
+
+        url = "https://data.cityofboston.gov/resource/bejm-5s9g.json"
         response = urllib.request.urlopen(url).read().decode("utf-8")
         r = json.loads(response)
         s = json.dumps(r, sort_keys=True, indent=2)
-        repo.dropPermanent("lost")
-        repo.createPermanent("lost")
-        repo['alice_bob.lost'].insert_many(r)
-
-        url = 'http://cs-people.bu.edu/lapets/591/examples/found.json'
-        response = urllib.request.urlopen(url).read().decode("utf-8")
-        r = json.loads(response)
-        s = json.dumps(r, sort_keys=True, indent=2)
-        repo.dropPermanent("found")
-        repo.createPermanent("found")
-        repo['alice_bob.found'].insert_many(r)
+        repo.dropPermanent("earnings")
+        repo.createPermanent("earnings")
+        repo['aydenbu_huangyh.earnings'].insert_many(r)
 
         repo.logout()
 
@@ -53,7 +77,7 @@ class example(dml.Algorithm):
          # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
+        repo.authenticate('aydenbu_huangyh', 'aydenbu_huangyh')
 
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
@@ -61,7 +85,7 @@ class example(dml.Algorithm):
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        this_script = doc.agent('alg:aydenbu_huangyh#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
@@ -78,12 +102,12 @@ class example(dml.Algorithm):
                 }
             )
 
-        lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
+        lost = doc.entity('dat:aydenbu_huangyh#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(lost, this_script)
         doc.wasGeneratedBy(lost, get_lost, endTime)
         doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
 
-        found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
+        found = doc.entity('dat:aydenbu_huangyh#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(found, this_script)
         doc.wasGeneratedBy(found, get_found, endTime)
         doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
