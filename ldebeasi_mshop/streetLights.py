dc68dd77da7e330c8ec2f573b6bde993941858ec
Liam DeBeasi <liamdebeasi@icloud.com>
2016-09-30 20:56:02 -0400

    add scripts to retrieve data sets

---
+++ ldebeasi_mshop/streetLights.py
@@ -0,0 +1,115 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+
+class streetLights(dml.Algorithm):
+    contributor = 'alice_bob'
+    reads = []
+    writes = ['alice_bob.streetLights']
+
+    @staticmethod
+    def execute(trial = False):
+        '''Retrieve Boston Street Light Locations'''
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+
+        repo.authenticate("alice_bob", "alice_bob")
+
+        hasMore = True
+        offset = 0
+        first = True
+
+        # Page through all data so we don't make our computer melt
+        while hasMore:
+            url = 'https://data.cityofboston.gov/resource/fbdp-b7et.json?$$app_token=' + dml.auth['bostonData'] + '&$order=:id&$limit=10000&$offset=' + str(offset)
+
+            response = urllib.request.urlopen(url).read().decode("utf-8")
+            r = json.loads(response)
+
+            # If first time loading data, only create data set after we get data
+            if first:
+                repo.dropPermanent("streetLights")
+                repo.createPermanent("streetLights")
+                first = False
+
+            # We have reached the end of the dataset
+            if r == []:
+                hasMore = False
+                break
+            # There is more data to be had -- Keep going
+            else:
+                offset += 10000
+
+            s = json.dumps(r, sort_keys=True, indent=2)
+
+            repo['alice_bob.streetLights'].insert_many(r)
+
+        repo.logout()
+
+        endTime = datetime.datetime.now()
+
+        return {"start":startTime, "end":endTime}
+
+    @staticmethod
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        '''
+        Create the provenance document describing everything happening
+        in this script. Each run of the script will generate a new
+        document describing that invocation event.
+        '''
+
+         # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('alice_bob', 'alice_bob')
+
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+        this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_found, this_script)
+        doc.wasAssociatedWith(get_lost, this_script)
+        doc.usage(get_found, resource, startTime, None,
+                {prov.model.PROV_TYPE:'ont:Retrieval',
+                 'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+                }
+            )
+        doc.usage(get_lost, resource, startTime, None,
+                {prov.model.PROV_TYPE:'ont:Retrieval',
+                 'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
+                }
+            )
+
+        lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(lost, this_script)
+        doc.wasGeneratedBy(lost, get_lost, endTime)
+        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+
+        found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(found, this_script)
+        doc.wasGeneratedBy(found, get_found, endTime)
+        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+
+        repo.record(doc.serialize()) # Record the provenance document.
+        repo.logout()
+
+        return doc
+
+streetLights.execute()
+#doc = threeOneOne.provenance()
+#print(doc.get_provn())
+#print(json.dumps(json.loads(doc.serialize()), indent=4))
+
+## eof
