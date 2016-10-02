b1f45d7b401181c45c92561fa5654df13ea48ca3
ktango <ktan@bu.edu>
2016-10-02 16:57:24 -0400

    Trying PDF scraper

--- ktan_ngurung/test.py
+++ ktan_ngurung/test.py
@@ -1,4 +1,9 @@
 import pdfquery 
+from xml.etree.ElementTree import ElementTree 
 
-pdf = pdfquery.PDFQuery("https://github.com/ktango/course-2016-fal-proj/blob/master/data-files/2014-bluebook-ridership.pdf")
+pdf = pdfquery.PDFQuery("2014-bluebook-ridership.pdf")
 pdf.load()
+pdf.tree.write("test.xml", pretty_print=True, encoding="utf-8")
+tree = ElementTree()
+tree.parse("test.xml")
+print(tree)
