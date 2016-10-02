a93ef453af0ea0056675b6a740cdc3f95715ecdb
ktango <ktan@bu.edu>
2016-10-02 19:00:27 -0400

    Updated ridership JSON and removed unnecessary files

--- ktan_ngurung/test.py
+++
@@ -1,9 +0,0 @@
-import pdfquery 
-from xml.etree.ElementTree import ElementTree 
-
-pdf = pdfquery.PDFQuery("2014-bluebook-ridership.pdf")
-pdf.load()
-pdf.tree.write("test.xml", pretty_print=True, encoding="utf-8")
-tree = ElementTree()
-tree.parse("test.xml")
-print(tree)
