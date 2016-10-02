db1da06fd6b316cbfa5e6096ca1f8cb6e78c46e5
hyh3898 <huangyh@bu.edu>
2016-10-01 22:24:12 -0400

    Merge collections

--- aydenbu_huangyh/merge_school_garden.py
+++ aydenbu_huangyh/merge_school_garden.py
@@ -11,9 +11,9 @@ from helpers import *
 
 class merge(dml.Algorithm):
     contributor = 'aydenbu_huangyh'
-    reads = ['aydenbu_huangyh.zip_hospitals_count',
-             'aydenbu_huangyh.zip_Healthycornerstores_count']
-    writes = ['aydenbu_huangyh.zip_health']
+    reads = ['aydenbu_huangyh.zip_PublicSchool_count',
+             'aydenbu_huangyh.zip_communityGardens_count']
+    writes = ['aydenbu_huangyh.zip_public']
 
     @staticmethod
     def execute(trial = False):
@@ -24,36 +24,36 @@ class merge(dml.Algorithm):
         repo = openDb(getAuth("db_username"), getAuth("db_password"))
 
         # Get the collections
-        hospitals = repo['aydenbu_huangyh.zip_hospitals_count']
-        stores = repo['aydenbu_huangyh.zip_Healthycornerstores_count']
+        gardens = repo['aydenbu_huangyh.zip_communityGardens_count']
+        schools = repo['aydenbu_huangyh.zip_PublicSchool_count']
 
 
         # For every document in hospitals zip find the number of store that associate with that zip
-        zip_health = []
-        for document in hospitals.find():
-            stores_count = stores.find_one({'_id': document['_id']}, {'_id': False, 'value.numofStore': True})
-            if stores_count is None:
+        zip_public = []
+        for document in gardens.find():
+            schools_count = schools.find_one({'_id': document['_id']}, {'_id': False, 'value.numofSchool': True})
+            if schools_count is None:
                 zip = {'_id': document['_id'],
                         'value': {
-                            'numofHospital': document['value']['numofHospital'],
-                            'numofStore': 0.0}  # Assign the 0 to the num if there is no result
+                            'numofGarden': document['value']['numofGarden'],
+                            'numofSchool': 0.0}  # Assign the 0 to the num if there is no result
                        }
-                zip_health.append(zip)
+                zip_public.append(zip)
                 continue
             else:
                 zip = {'_id': document['_id'],
                         'value': {
-                            'numofHospital': document['value']['numofHospital'],
-                            'numofStore': stores_count['value']['numofStore']}
+                            'numofGarden': document['value']['numofGarden'],
+                            'numofSchool': schools_count['value']['numofSchool']}
                        }
-                zip_health.append(zip)
+                zip_public.append(zip)
         ''''''''''''''''''''''''''''''''''''''''''''''''
 
 
         # Create a new collection and insert the result data set
-        repo.dropPermanent("zip_health")
-        repo.createPermanent("zip_health")
-        repo['aydenbu_huangyh.zip_health'].insert_many(zip_health)
+        repo.dropPermanent("zip_public")
+        repo.createPermanent("zip_public")
+        repo['aydenbu_huangyh.zip_public'].insert_many(zip_public)
 
         repo.logout()
         endTime = datetime.datetime.now()
@@ -82,46 +82,46 @@ class merge(dml.Algorithm):
         '''
         '''
         # The agent
-        this_script = doc.agent('alg:aydenbu_huangyh#merge_store_hospital',
+        this_script = doc.agent('alg:aydenbu_huangyh#merge_school_garden',
                                 {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
 
         # The source entity
-        store_source = doc.entity('dat:healthy_corner_store_count',
-                              {'prov:label': 'HealthyCorner Store Count', prov.model.PROV_TYPE: 'ont:DataResource',
+        school_source = doc.entity('dat:public_school_count',
+                              {'prov:label': 'Public School Count', prov.model.PROV_TYPE: 'ont:DataResource',
                                'ont:Extension': 'json'})
-        hospital_source = doc.entity('dat:hospital_count',
-                                  {'prov:label': 'Hospital Count', prov.model.PROV_TYPE: 'ont:DataResource',
+        garden_source = doc.entity('dat:community_garden_count',
+                                  {'prov:label': 'Community Garden Count', prov.model.PROV_TYPE: 'ont:DataResource',
                                    'ont:Extension': 'json'})
 
         # The activity
-        get_zip_health = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime,
-                                               {prov.model.PROV_LABEL: "Merge the numbers of hospital and HealthyStore in each zip"})
+        get_zip_public = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime,
+                                               {prov.model.PROV_LABEL: "Merge the numbers of garden and school in each zip"})
 
         # The activity is associated with the agent
-        doc.wasAssociatedWith(get_zip_health, this_script)
+        doc.wasAssociatedWith(get_zip_public, this_script)
 
         # Usage of the activity: Source Entity
-        doc.usage(get_zip_health, store_source, startTime, None,
+        doc.usage(get_zip_public, school_source, startTime, None,
                   {prov.model.PROV_TYPE: 'ont:Computation'})
-        doc.usage(get_zip_health, hospital_source, startTime, None,
+        doc.usage(get_zip_public, garden_source, startTime, None,
                   {prov.model.PROV_TYPE: 'ont:Computation'})
 
         # The Result Entity
-        zip_health = doc.entity('dat:aydenbu_huangyh#zip_health',
-                                            {prov.model.PROV_LABEL: 'Zip Health',
+        zip_public = doc.entity('dat:aydenbu_huangyh#zip_public',
+                                            {prov.model.PROV_LABEL: 'Zip Public Building',
                                              prov.model.PROV_TYPE: 'ont:DataSet'})
 
         # Result Entity was attributed to the agent
-        doc.wasAttributedTo(zip_health, this_script)
+        doc.wasAttributedTo(zip_public, this_script)
 
         # Result Entity was generated by the activity
-        doc.wasGeneratedBy(zip_health, get_zip_health, endTime)
+        doc.wasGeneratedBy(zip_public, get_zip_public, endTime)
 
         # Result Entity was Derived From Source Entity
-        doc.wasDerivedFrom(zip_health, store_source, get_zip_health, get_zip_health,
-                           get_zip_health)
-        doc.wasDerivedFrom(zip_health, hospital_source, get_zip_health, get_zip_health,
-                           get_zip_health)
+        doc.wasDerivedFrom(zip_public, school_source, get_zip_public, get_zip_public,
+                           get_zip_public)
+        doc.wasDerivedFrom(zip_public, garden_source, get_zip_public, get_zip_public,
+                           get_zip_public)
 
         repo.record(doc.serialize())
         repo.logout()
