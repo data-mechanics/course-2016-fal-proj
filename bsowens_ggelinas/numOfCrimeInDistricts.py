cdad3084e029c9eee0c47905a0fafed7ce668956
George Gelinas <ggelinas@bu.edu>
2016-10-03 16:28:46 -0400

    edited numOfCrimeInDistricts

---
+++ bsowens_ggelinas/numOfCrimeInDistricts.py
@@ -0,0 +1,29 @@
+import urllib.request
+import urllib.parse
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+
+class numOfCrimeInDistrics(dml.Algorithm):
+    contributor = 'bsowens_ggelinas'
+    reads = ['bsowens_ggelinas.stations',
+             'bsowens_ggelinas.incidents',
+             'bsowens_ggelinas.property',
+             'bsoquitwens_ggelinas.fio',
+             'bsowens_ggelinas.hospitals']
+    writes = ['bsowens_ggelinas.stations',
+              'bsowens_ggelinas.incidents',
+              'bsowens_ggelinas.property',
+              'bsoquitwens_ggelinas.fio',
+              'bsowens_ggelinas.hospitals']
+
+    @staticmethod
+    def execute(trial = False):
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('bsowens_ggelinas', 'bsowens_ggelinas')
