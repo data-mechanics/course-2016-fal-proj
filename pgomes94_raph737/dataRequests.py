8b8e11b2e130e2cb03f7fc7d810950d65c54a929
Raphib737 <raph737@bu.edu>
2016-10-02 20:05:09 -0400

    added MBTA data gathering algorithm. Currently can not get all but got them in a csv before getting rate limited

--- pgomes94_raph737/dataRequests.py
+++ pgomes94_raph737/dataRequests.py
@@ -1,9 +1,53 @@
-import requests, json,pprint
+import requests, json,pprint,csv
 
-mbtaStops = requests.get("http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key=wX9NwuHnZU2ToO7GmGR9uw&lat=42.346961&lon=-71.076640&format=json")
+class dataRequests():
+	def get_mbta_request_string(api_key,lat,lon):
+		return "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key="+api_key+"&lat="+lat+"&lon="+lon+"&format=json"
 
-data = mbtaStops.json()
+	stops = {}
+	pp = pprint.PrettyPrinter(indent=4)
+
+	api_key = '8xE5xPe7rESr2gzOaD06Pg'
+	lat = "42.346961"
+	lon = "-71.076640"
+	request_string = "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key="+api_key+"&lat="+lat+"&lon="+lon+"&format=json"
+	mbta_stops = requests.get(request_string)
+
+	data = mbta_stops.json()
+	temp_stops = [(dat['stop_id'],dat['stop_name'],dat['stop_lat'],dat['stop_lon'])for dat in data['stop']]
+	has_more_stops = True
+	count = 0
+
+	with open('mbtaStops.csv', 'w') as f:
+		wr = csv.writer(f);
+		while has_more_stops:
+
+			if len(temp_stops) == 0:
+				has_more_stops = False
+				break
+
+			for stop in temp_stops:
+				print(len(temp_stops))
+				if len(temp_stops) == 0:
+					has_more_stops = False
+					break
+
+				if (stop[2],stop[3]) in stops:
+					temp_stops.remove(stop)
+				else:
+					lat = stop[2]
+					lon = stop[3]
+					stops[(lat,lon)] = True
+					new_dat = (stop[0],stop[1],lat,lon)
+
+					wr.writerow(new_dat) 
+					temp_stops.remove(new_dat)
 
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(data)
+					new_request = get_mbta_request_string(api_key,lat,lon)
+					next_data = requests.get(new_request).json()['stop']
+					new_temp_stops = [(dat['stop_id'],dat['stop_name'],dat['stop_lat'],dat['stop_lon']) for dat in next_data]
+					for x in new_temp_stops:
+						if (x[2],x[3]) not in stops.keys():
+							temp_stops.append(x)
+	print("Completed!")
