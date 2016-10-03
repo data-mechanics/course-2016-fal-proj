471731fa1837434f0d216fd3a51b57ac44dbaf21
Patrick Gomes <pgomes94@wireless1x-155-41-101-54.bu.edu>
2016-10-03 19:01:50 -0400

    cleaned up database entries

--- pgomes94_raph737/dataRequests.py
+++ pgomes94_raph737/dataRequests.py
@@ -5,7 +5,6 @@ import csv
 import dml
 import prov.model
 import datetime
-import urllib.request
 
 class dataRequests(dml.Algorithm):
 	contributor = 'pgomes94_raph737'
@@ -22,9 +21,12 @@ class dataRequests(dml.Algorithm):
 		hospital_data_request = requests.get("https://data.cityofboston.gov/resource/u6fv-m8v4.json?$$app_token=" + dml.auth['city_of_boston'])
 		if hospital_data_request.status_code == 200:
 			print("Successfully requested hopsital data.")
-			hospital_locations = {}
+			hospital_locations = []
 			for json_data in hospital_data_request.json():
-				hospital_locations[json_data['name'].replace('.','')] = json_data['location']['coordinates']
+				hospital_locations.append({
+					'identifier': json_data['name'].replace('.',''),
+					'location': (json_data['location']['coordinates'][1],json_data['location']['coordinates'][0])
+					})
 			return hospital_locations
 		else:
 			print("Error: Hospital Request failed. Status code: {}".format(hospital_data_request.status_code))
@@ -36,14 +38,17 @@ class dataRequests(dml.Algorithm):
 		if traffic_data_request.status_code == 200 and traffic_location_request.status_code == 200:
 			print("Successfully requested traffic location data.")
 			id_to_location = {}
-			traffic_locations = {}
+			traffic_locations = []
 
 			for json_data in traffic_location_request.json():
-				id_to_location[json_data['uuid']] = (json_data['x'], json_data['y'])
+				id_to_location[json_data['uuid']] = (json_data['y'], json_data['x'])
 
 			for json_data in traffic_data_request.json():
 				try:
-					traffic_locations[json_data['uuid']] = id_to_location[json_data['uuid']]
+					traffic_locations.append({
+						'identifier':json_data['uuid'],
+						'location': id_to_location[json_data['uuid']]
+						})
 				except:
 					pass
 			return traffic_locations
@@ -55,10 +60,13 @@ class dataRequests(dml.Algorithm):
 		crime_data_request = requests.get("https://data.cityofboston.gov/resource/29yf-ye7n.json?$$app_token=" + dml.auth['city_of_boston'])
 		if crime_data_request.status_code == 200:
 			print("Successfully requested crime location data.")
-			crime_locations = {}
+			crime_locations = []
 			for json_data in crime_data_request.json():
 				try:
-					crime_locations[json_data['incident_number']] = (json_data["long"], json_data["lat"])
+					crime_locations.append({
+						'identifier':json_data['incident_number'],
+						'location': (json_data["lat"], json_data["long"])
+						})
 				except:
 					pass
 			return crime_locations
@@ -70,69 +78,19 @@ class dataRequests(dml.Algorithm):
 		police_station_request = requests.get("https://data.cityofboston.gov/resource/pyxn-r3i2.json?$$app_token=" + dml.auth['city_of_boston'])
 		if police_station_request.status_code == 200:
 			print("Successfully requested police station locations")
-			police_station_locations = {}
+			police_station_locations = []
 			for json_data in police_station_request.json():
-				police_station_locations[json_data['name']] = (json_data['location']['coordinates'][0],json_data['location']['coordinates'][0])
+				police_station_locations.append({
+					'identifier': json_data['name'],
+					'location': (json_data['location']['coordinates'][1],json_data['location']['coordinates'][0])
+					})
 			return police_station_locations
 		else:
 			print("Error: Police requests failed. Status code: {}".format(police_station_request.status_code))
 			return -1
 
-	def get_mbta_stop_locations_web():
-		stops = {}
-		pp = pprint.PrettyPrinter(indent=4)
-
-		api_key = dml.auth['mbta']
-		lat = "42.346961"
-		lon = "-71.076640"
-		request_string = get_mbta_request_string(api_key,lat,lon)
-		mbta_stops = requests.get(request_string)
-
-		data = mbta_stops.json()
-		temp_stops = [(dat['stop_id'],dat['stop_name'],dat['stop_lat'],dat['stop_lon'])for dat in data['stop']]
-		has_more_stops = True
-		count = 0
-
-		with open('mbtaStops.csv', 'w') as f:
-			wr = csv.writer(f);
-			while has_more_stops:
-
-				if len(temp_stops) == 0:
-					has_more_stops = False
-					break
-
-				for stop in temp_stops:
-					print(len(temp_stops))
-					if len(temp_stops) == 0:
-						has_more_stops = False
-						break
-
-					if (stop[2],stop[3]) in stops:
-						temp_stops.remove(stop)
-					else:
-						lat = stop[2]
-						lon = stop[3]
-						stops[(lat,lon)] = True
-						new_dat = (stop[0],stop[1],lat,lon)
-
-						wr.writerow(new_dat) 
-						temp_stops.remove(new_dat)
-
-						new_request = get_mbta_request_string(api_key,lat,lon)
-						next_data = requests.get(new_request).json()['stop']
-						new_temp_stops = [(dat['stop_id'],dat['stop_name'],dat['stop_lat'],dat['stop_lon']) for dat in next_data]
-						for x in new_temp_stops:
-							if (x[2],x[3]) not in stops.keys():
-								temp_stops.append(x)
-		print("Completed getting MBTA stops! Data can be found in mbtaStops.csv")
-		return {}
-
-	#Helper Function for MBTA querying
-	def get_mbta_request_string(api_key,lat,lon):
-		return "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key="+api_key+"&lat="+lat+"&lon="+lon+"&format=json"
-
 	def get_mbta_stop_locations_csv():
-		mbta_stops = {}
+		mbta_stops = []
 		mbta_stop_request = requests.get("http://datamechanics.io/data/pgomes94_raph737/stops.csv")
 		if mbta_stop_request.status_code == 200:
 			first = True
@@ -145,7 +103,10 @@ class dataRequests(dml.Algorithm):
 						# last line, end case
 						continue
 					try:
-						mbta_stops[parts[0]] = {"stop_name":parts[2],"lat":parts[4],"lon":parts[5]}
+						mbta_stops.append({
+							'identifier':parts[0],
+							'location': (parts[4], parts[5])
+							})
 					except:
 						print("Error: Failed to parse line: {}".format(line))
 			print("Successfully parsed MBTA stops csv.")
@@ -156,40 +117,40 @@ class dataRequests(dml.Algorithm):
 
 	@staticmethod
 	def execute(trial = False):
-		startTime = datetime.datetime.now()
+		start_time = datetime.datetime.now()
 
 		client = dml.pymongo.MongoClient()
 		repo = client.repo
 		repo.authenticate('pgomes94_raph737', 'pgomes94_raph737')
-		'''
+		
 		hospital_locations = dataRequests.get_hospital_locations()
 		repo.dropPermanent("hospital_locations")
 		repo.createPermanent("hospital_locations")
-		repo['pgomes94_raph737.hospital_locations'].insert(hospital_locations)
+		repo['pgomes94_raph737.hospital_locations'].insert_many(hospital_locations)
 		
 		traffic_locations = dataRequests.get_traffic_locations()
 		repo.dropPermanent("traffic_locations")
 		repo.createPermanent("traffic_locations")
-		repo['pgomes94_raph737.traffic_locations'].insert(traffic_locations)
+		repo['pgomes94_raph737.traffic_locations'].insert_many(traffic_locations)
 
 		crime_locations = dataRequests.get_crime_locations()
 		repo.dropPermanent("crime_locations")
 		repo.createPermanent("crime_locations")
-		repo['pgomes94_raph737.crime_locations'].insert(crime_locations)
+		repo['pgomes94_raph737.crime_locations'].insert_many(crime_locations)
 
 		police_station_locations = dataRequests.get_police_station_locations()
 		repo.dropPermanent("police_station_locations")
 		repo.createPermanent("police_station_locations")
-		repo['pgomes94_raph737.police_station_locations'].insert(police_station_locations)
-		'''
+		repo['pgomes94_raph737.police_station_locations'].insert_many(police_station_locations)
+		
 		mbta_stop_locations = dataRequests.get_mbta_stop_locations_csv()
 		repo.dropPermanent("mbta_stop_locations")
 		repo.createPermanent("mbta_stop_locations")
-		repo['pgomes94_raph737.mbta_stop_locations'].insert(mbta_stop_locations)
+		repo['pgomes94_raph737.mbta_stop_locations'].insert_many(mbta_stop_locations)
 		
 		repo.logout()
 
-		endTime = datetime.datetime.now()
+		end_time = datetime.datetime.now()
 
 	@staticmethod
 	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
