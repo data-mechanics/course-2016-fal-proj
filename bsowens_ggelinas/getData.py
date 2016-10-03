ee44a3814819f56fe3753d0ce4c7a41e9db739b9
George Gelinas <ggelinas@bu.edu>
2016-10-03 14:26:08 -0400

    Added Provenance

--- bsowens_ggelinas/getData.py
+++ bsowens_ggelinas/getData.py
@@ -11,7 +11,7 @@ class getData(dml.Algorithm):
     writes = ['bsowens_ggelinas.stations',
               'bsowens_ggelinas.incidents',
               'bsowens_ggelinas.property',
-              'bsoquitwens_ggelinas.fio',
+              'bsowens_ggelinas.fio',
               'bsowens_ggelinas.hospitals']
 
     @staticmethod
@@ -62,6 +62,82 @@ class getData(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('bsowens_ggelinas', 'bsowens_ggelinas')
+
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont',
+                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+        this_script = doc.agent('alg:bsowens_ggelinas#getData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+
+        stations_info = doc.entity('bdp:pyxn-r3i2', {'prov:label':'District Police Stations', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        stations_getInfo = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime, {'prov:label':'Get District Police Stations Data'})
+        doc.wasAssociatedWith(stations_getInfo, this_script)
+        doc.usage(
+            stations_getInfo,
+            stations_info,
+            startTime,
+            None,
+            {prov.model.PROV_TYPE:'ont:Retrieval'}
+        )
+
+        incidents_info = doc.entity('bdp:29yf-ye7n',
+                                   {'prov:label': 'Crime Incidents Report', prov.model.PROV_TYPE: 'ont:DataResource',
+                                    'ont:Extension': 'json'})
+        incidents_getInfo = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime,
+                                        {'prov:label': 'Get Crime Incidents Report Data'})
+        doc.wasAssociatedWith(incidents_getInfo, this_script)
+        doc.usage(
+            incidents_getInfo,
+            incidents_info,
+            startTime,
+            None,
+            {prov.model.PROV_TYPE: 'ont:Retrieval'}
+        )
+
+        property_info = doc.entity('bdp:g5b5-xrwi',
+                                    {'prov:label': 'Property Assessment 2016', prov.model.PROV_TYPE: 'ont:DataResource',
+                                     'ont:Extension': 'json'})
+        property_getInfo = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime,
+                                         {'prov:label': 'Get Property Assessment 2016 Data'})
+        doc.wasAssociatedWith(property_getInfo, this_script)
+        doc.usage(
+            property_getInfo,
+            property_info,
+            startTime,
+            None,
+            {prov.model.PROV_TYPE: 'ont:Retrieval'}
+        )
+
+        fio_info = doc.entity('bdp:2pem-965w',
+                                   {'prov:label': 'Boston Police Department FIO', prov.model.PROV_TYPE: 'ont:DataResource',
+                                    'ont:Extension': 'json'})
+        fio_getInfo = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime,
+                                        {'prov:label': 'Get Boston Police Department FIO Data'})
+        doc.wasAssociatedWith(fio_getInfo, this_script)
+        doc.usage(
+            fio_getInfo,
+            fio_info,
+            startTime,
+            None,
+            {prov.model.PROV_TYPE: 'ont:Retrieval'}
+        )
+
+        hospitals_info = doc.entity('bdp:u6fv-m8v4',
+                              {'prov:label': 'Hospital Locations', prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+        hospitals_getInfo = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime,
+                                   {'prov:label': 'Get Hospital Locations Data'})
+        doc.wasAssociatedWith(hospitals_getInfo, this_script)
+        doc.usage(
+            hospitals_getInfo,
+            hospitals_info,
+            startTime,
+            None,
+            {prov.model.PROV_TYPE: 'ont:Retrieval'}
+        )
 
 
 getData.execute()
