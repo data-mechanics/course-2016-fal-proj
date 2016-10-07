574bd8f3d5da84e2593c023307ee4cba790571b1
Raphib737 <raph737@bu.edu>
2016-10-06 21:08:30 -0400

    Added provenance for last dataset

--- pgomes94_raph737/pysparkMapReduceJob.py
+++ pgomes94_raph737/pysparkMapReduceJob.py
@@ -5,6 +5,7 @@ import csv
 import dml
 import prov.model
 import datetime
+import uuid
 
 from sklearn.cluster import KMeans
 import numpy as np
@@ -64,7 +65,44 @@ class pysparkMapReduceJob(dml.Algorithm):
 
 	@staticmethod
 	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
-		pass
+		# Set up the database connection.
+		client = dml.pymongo.MongoClient()
+		repo = client.repo
+		repo.authenticate('pgomes94_raph737', 'pgomes94_raph737')
+
+		doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+		doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+		doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+		doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+		doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+		this_script = doc.agent('alg:pgomes94_raph737#pysparkMapReduceJob', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+
+		hospitals_resource = doc.entity('dat:pgomes94_raph737#hospital_locations', {prov.model.PROV_LABEL:'Hospital Locations', prov.model.PROV_TYPE:'ont:DataSet'})
+		proximity_clusters_resources = doc.entity('dat:pgomes94_raph737#proximity_clusters', {prov.model.PROV_LABEL:'Proximity Clusters', prov.model.PROV_TYPE:'ont:DataSet'})
+
+		get_hospital_locations = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+		get_proximity_clusters = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+		get_hospital_scores = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+		doc.wasAssociatedWith(get_hospital_locations, this_script)
+		doc.wasAssociatedWith(get_proximity_clusters, this_script)
+
+		doc.usage(get_hospital_locations,hospitals_resource,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
+		doc.usage(get_proximity_clusters,proximity_clusters_resources,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
+
+		hospital_scores = doc.entity('dat:pgomes94_raph737#hospital_scores', {prov.model.PROV_LABEL:'Crime Locations', prov.model.PROV_TYPE:'ont:DataSet'})
+		doc.wasAttributedTo(hospital_scores,this_script)
+		doc.wasGeneratedBy(hospital_scores,get_hospital_scores,endTime)
+		doc.wasDerivedFrom(hospital_scores,hospitals_resource,proximity_clusters_resources,get_hospital_locations,get_proximity_clusters)
+
+		repo.record(doc.serialize())
+		repo.logout()
+
+		return doc
+
 
 if __name__ == "__main__":
 	pysparkMapReduceJob.execute()
+	doc = pysparkMapReduceJob.provenance()
+	print(doc.get_provn())
+	print(json.dumps(json.loads(doc.serialize()), indent=4))
