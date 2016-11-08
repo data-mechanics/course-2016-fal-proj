9bc357e5ef138912b14571975b75b4fe39d93e8f
Sanam Patel <patels326@gmail.com>
2016-11-08 18:53:54 -0500

    calculated average crimes and firearms recovered

--- manda094_nwg_patels95/firearms_crimes_analysis.py
+++ manda094_nwg_patels95/firearms_crimes_analysis.py
@@ -5,10 +5,24 @@ import prov.model
 import datetime
 import uuid
 
-class crimes(dml.Algorithm):
+class firearms_crimes_analysis(dml.Algorithm):
     contributor = 'manda094_nwg_patels95'
     reads = ['manda094_nwg_patels95.firearm_recovery']
     writes = []
+
+    def avg(x): # Average
+        return sum(x)/len(x)
+
+    def stddev(x): # Standard deviation.
+        m = avg(x)
+        return sqrt(sum([(xi-m)**2 for xi in x])/len(x))
+
+    def cov(x, y): # Covariance.
+        return sum([(xi-avg(x))*(yi-avg(y)) for (xi,yi) in zip(x,y)])/len(x)
+
+    def corr(x, y): # Correlation coefficient.
+        if stddev(x)*stddev(y) != 0:
+            return cov(x, y)/(stddev(x)*stddev(y))
 
     @staticmethod
     def execute(trial = False):
@@ -18,6 +32,24 @@ class crimes(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('manda094_nwg_patels95', 'manda094_nwg_patels95')
+
+        crimes = []
+        firearmsRecovered = []
+        for data in repo['manda094_nwg_patels95.firearm_recovery'].find():
+            try:
+                crimes.append(data["totalcrimes"])
+            except KeyError:
+                # add 0 to array if "totalcrimes" key does not exist
+                crimes.append(0)
+            firearmsRecovered.append(data["totalgunsrecovered"])
+
+        print("Crimes")
+        print(crimes)
+        print("Guns Recovered")
+        print(firearmsRecovered)
+
+        print(firearms_crimes_analysis.avg(crimes))
+        print(firearms_crimes_analysis.avg(firearmsRecovered))
 
         repo.logout()
 
@@ -36,7 +68,7 @@ class crimes(dml.Algorithm):
 
         return doc
 
-crimes.execute()
+firearms_crimes_analysis.execute()
 # doc = crimes.provenance()
 # print(doc.get_provn())
 # print(json.dumps(json.loads(doc.serialize()), indent=4))
