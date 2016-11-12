50f054dece3200ecd5dcf2498d2e6ed5bdad41d0
Mark Bestavros <mark.bestavros@live.com>
2016-11-12 17:05:16 -0500

    Add GIS code to getData.py

--- alaw_markbest_tyroneh/getData.py
+++ alaw_markbest_tyroneh/getData.py
@@ -160,6 +160,36 @@ class getData(dml.Algorithm):
             repo.createPermanent('TimedBuses')
             repo['alaw_markbest_tyroneh.TimedBuses'].insert_many(all_buses)  
 
+
+	#GIS data/shapefiles urls, converts them to python dictionaries
+	gisURLs = {"CensusPopulation":'http://wsgw.mass.gov/data/gispub/shape/census2010/CENSUS2010TOWNS_SHP.zip'}
+	gisTowns = ['BOSTON','BROOKLINE','CAMBRIDGE','SOMERVILLE']
+
+	for key in gisURLs:
+        	url = gisURLs[key]
+
+	        # Get the data from the server.
+        	response = urllib.request.urlopen(url)
+
+	        # Unzip the file into the working directory.
+        	zip_ref = zipfile.ZipFile(io.BytesIO(response.read()))
+	        zip_ref.extractall("./")
+        	zip_ref.close()
+
+	        # Read the file into the Python shapefile library.
+        	sf = shapefile.Reader("CENSUS2010TOWNS_POLY")
+
+	        # Pull out the specific records for the four areas of interest, listed in gisTowns above.
+        	# NOTE: The attribute at index 1 of any record is the uppercase town name.
+	        # stores dictionary with town name as key and 2010 population as data
+
+        	boston_area = {x[1]:x[9] for x in sf.iterRecords() if x[1] in gisTowns}
+
+	        # Set up the database connection
+        	repo.dropPermanent(key)
+	        repo.createPermanent(key)
+        	repo['alaw_markbest_tyroneh.'+key].insert_many(boston_area)
+
         repo.logout()
         endTime = datetime.datetime.now()
         
