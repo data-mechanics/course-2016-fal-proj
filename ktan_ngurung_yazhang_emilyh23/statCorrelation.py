6e302dd2cf5b7d9805e88aa3804a68f850cc600d
Nisa Gurung <nisagurung@dhcp-wifi-8021x-168-122-15-12.bu.edu>
2016-11-13 15:32:14 -0500

    extends dml.algorithm class

--- ktan_ngurung_yazhang_emilyh23/statCorrelation.py
+++ ktan_ngurung_yazhang_emilyh23/statCorrelation.py
@@ -15,89 +15,99 @@ import warnings
 import numpy 
 warnings.filterwarnings("ignore")
 
-def statCorrelation():
-    '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
-    startTime = datetime.datetime.now()
+class statCorrelation(dml.Algorithm):
+    contributor = 'ktan_ngurung_yazhang_emilyh23'
+    reads = ['ktan_ngurung_yazhang_emilyh23.zipcodeRatings']
+    writes = [] 
 
-    # Set up the database connection.
-    client = dml.pymongo.MongoClient()
-    repo = client.repo
-    repo.authenticate('ktan_ngurung_yazhang_emilyh23', 'ktan_ngurung_yazhang_emilyh23')
+    @staticmethod
+    def execute():
+        '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
+        startTime = datetime.datetime.now()
 
-    # Get bus stop and college location data
-    zipcode_ratings = repo.ktan_ngurung_yazhang_emilyh23.zipcodeRatings.find_one() 
-    zipcode_ratings_df = pd.DataFrame(zipcode_ratings)
-    del zipcode_ratings_df['_id']
-    zipcode_ratings_df = zipcode_ratings_df.transpose()
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('ktan_ngurung_yazhang_emilyh23', 'ktan_ngurung_yazhang_emilyh23')
 
-    bigBelly_star = list(zipcode_ratings_df['bigBelly_star'])
-    bus_star = list(zipcode_ratings_df['bus_star'])
-    college_star = list(zipcode_ratings_df['college_star'])
-    hubway_star = list(zipcode_ratings_df['hubway_star'])
-    station_star = list(zipcode_ratings_df['station_star'])
+        # Get bus stop and college location data
+        zipcode_ratings = repo.ktan_ngurung_yazhang_emilyh23.zipcodeRatings.find_one() 
+        zipcode_ratings_df = pd.DataFrame(zipcode_ratings)
+        del zipcode_ratings_df['_id']
+        zipcode_ratings_df = zipcode_ratings_df.transpose()
 
-    master_dict = {'bigBelly_star':bigBelly_star, 'bus_star':bus_star, 'college_star':college_star, 'hubway_star':hubway_star, 'station_star':station_star}
-    bigBelly_cc, bus_cc, college_cc, hubway_cc, station_cc = {}, {}, {}, {}, {}
+        bigBelly_star = list(zipcode_ratings_df['bigBelly_star'])
+        bus_star = list(zipcode_ratings_df['bus_star'])
+        college_star = list(zipcode_ratings_df['college_star'])
+        hubway_star = list(zipcode_ratings_df['hubway_star'])
+        station_star = list(zipcode_ratings_df['station_star'])
 
-    # Calculate the Pearson product-moment correlation coefficient for each category to every other category 
+        master_dict = {'bigBelly_star':bigBelly_star, 'bus_star':bus_star, 'college_star':college_star, 'hubway_star':hubway_star, 'station_star':station_star}
+        bigBelly_cc, bus_cc, college_cc, hubway_cc, station_cc = {}, {}, {}, {}, {}
 
-    bigBelly_cc['bus_cc'] = pearsonr(master_dict['bus_star'], master_dict['bigBelly_star'])
-    bigBelly_cc['college_cc'] = pearsonr(master_dict['college_star'], master_dict['bigBelly_star'])
-    bigBelly_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['bigBelly_star'])
-    bigBelly_cc['station_cc'] = pearsonr(master_dict['station_star'], master_dict['bigBelly_star'])
-    print('bigBelly_cc', bigBelly_cc)
+        # Calculate the Pearson product-moment correlation coefficient for each category to every other category 
 
-    bus_cc['college_cc'] = pearsonr(master_dict['college_star'], master_dict['bus_star'])
-    bus_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['bus_star'])
-    bus_cc['station_cc'] = pearsonr(master_dict['station_star'], master_dict['bus_star'])
-    print('bus_cc', bus_cc)
+        bigBelly_cc['bus_cc'] = pearsonr(master_dict['bus_star'], master_dict['bigBelly_star'])
+        bigBelly_cc['college_cc'] = pearsonr(master_dict['college_star'], master_dict['bigBelly_star'])
+        bigBelly_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['bigBelly_star'])
+        bigBelly_cc['station_cc'] = pearsonr(master_dict['station_star'], master_dict['bigBelly_star'])
+        print('bigBelly_cc', bigBelly_cc)
 
-    college_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['college_star'])
-    college_cc['station_cc'] = pearsonr(master_dict['station_star'], master_dict['college_star'])
-    print('college_cc', college_cc)
+        bus_cc['college_cc'] = pearsonr(master_dict['college_star'], master_dict['bus_star'])
+        bus_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['bus_star'])
+        bus_cc['station_cc'] = pearsonr(master_dict['station_star'], master_dict['bus_star'])
+        print('bus_cc', bus_cc)
 
-    station_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['station_star'])
-    print('station_cc', station_cc)
+        college_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['college_star'])
+        college_cc['station_cc'] = pearsonr(master_dict['station_star'], master_dict['college_star'])
+        print('college_cc', college_cc)
 
-    bigBelly_star_dict = OrderedDict(zipcode_ratings_df['bigBelly_star'])
-    bus_star_dict = OrderedDict(zipcode_ratings_df['bus_star'])
-    college_star_dict = OrderedDict(zipcode_ratings_df['college_star'])
-    hubway_dict = OrderedDict(zipcode_ratings_df['hubway_star'])
-    station_dict = OrderedDict(zipcode_ratings_df['station_star'])
+        station_cc['hubway_cc'] = pearsonr(master_dict['hubway_star'], master_dict['station_star'])
+        print('station_cc', station_cc)
 
-    colors = np.random.rand(22)
+        bigBelly_star_dict = OrderedDict(zipcode_ratings_df['bigBelly_star'])
+        bus_star_dict = OrderedDict(zipcode_ratings_df['bus_star'])
+        college_star_dict = OrderedDict(zipcode_ratings_df['college_star'])
+        hubway_dict = OrderedDict(zipcode_ratings_df['hubway_star'])
+        station_dict = OrderedDict(zipcode_ratings_df['station_star'])
 
-    fig = plt.figure()
-    gs = gridspec.GridSpec(5, 2)
+        colors = np.random.rand(22)
 
-    ax1 = fig.add_subplot(gs[0,:])
-    ax1.set_title('Big Belly Star Ratings')
-    ax1.scatter(range(len(bigBelly_star_dict.keys())), [int(v) for v in bigBelly_star_dict.values()], c=colors)
-    plt.xticks(range(len(bigBelly_star_dict.keys())))
+        fig = plt.figure()
+        gs = gridspec.GridSpec(5, 2)
 
-    ax2 = fig.add_subplot(gs[1,:])
-    ax2.set_title('Bus Star Ratings')
-    ax2.scatter(range(len(bus_star_dict.keys())), [int(v) for v in bus_star_dict.values()], c=colors)
-    plt.xticks(range(len(bus_star_dict.keys())))
+        ax1 = fig.add_subplot(gs[0,:])
+        ax1.set_title('Big Belly Star Ratings')
+        ax1.scatter(range(len(bigBelly_star_dict.keys())), [int(v) for v in bigBelly_star_dict.values()], c=colors)
+        plt.xticks(range(len(bigBelly_star_dict.keys())))
 
-    ax3 = fig.add_subplot(gs[2,:])
-    ax3.set_title('College Star Ratings')
-    ax3.scatter(range(len(college_star_dict.keys())), [int(v) for v in college_star_dict.values()], c=colors)
-    plt.xticks(range(len(college_star_dict.keys())))
+        ax2 = fig.add_subplot(gs[1,:])
+        ax2.set_title('Bus Star Ratings')
+        ax2.scatter(range(len(bus_star_dict.keys())), [int(v) for v in bus_star_dict.values()], c=colors)
+        plt.xticks(range(len(bus_star_dict.keys())))
 
-    ax4 = fig.add_subplot(gs[3,:])
-    ax4.set_title('Hubway Star Ratings')
-    ax4.scatter(range(len(hubway_dict.keys())), [int(v) for v in hubway_dict.values()], c=colors)
-    plt.xticks(range(len(hubway_dict.keys())))
+        ax3 = fig.add_subplot(gs[2,:])
+        ax3.set_title('College Star Ratings')
+        ax3.scatter(range(len(college_star_dict.keys())), [int(v) for v in college_star_dict.values()], c=colors)
+        plt.xticks(range(len(college_star_dict.keys())))
 
-    ax5 = fig.add_subplot(gs[4,:])
-    ax5.set_title('Station Star Ratings')
-    ax5.scatter(range(len(station_dict.keys())), [int(v) for v in station_dict.values()], c=colors)
-    plt.xticks(range(len(station_dict.keys())))
+        ax4 = fig.add_subplot(gs[3,:])
+        ax4.set_title('Hubway Star Ratings')
+        ax4.scatter(range(len(hubway_dict.keys())), [int(v) for v in hubway_dict.values()], c=colors)
+        plt.xticks(range(len(hubway_dict.keys())))
 
-    plt.tight_layout()
-    plt.show()
+        ax5 = fig.add_subplot(gs[4,:])
+        ax5.set_title('Station Star Ratings')
+        ax5.scatter(range(len(station_dict.keys())), [int(v) for v in station_dict.values()], c=colors)
+        plt.xticks(range(len(station_dict.keys())))
 
-statCorrelation()
+        plt.tight_layout()
+        plt.show()
+
+    @staticmethod
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        pass 
+
+statCorrelation.execute()
 
 ## eof
