8d6bd2249b9396ffa65dfcdd622893f5126aeafe
Raphib737 <raph737@bu.edu>
2016-11-13 22:32:32 -0500

    Made provenance for getError.py

--- mgerakis_pgomes94_raph737/getError.py
+++ mgerakis_pgomes94_raph737/getError.py
@@ -91,56 +91,20 @@ class getError(dml.Algorithm):
 		doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
 		doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-		this_script = doc.agent('alg:mgerakis_pgomes94_raph737#dataRequests', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+		this_script = doc.agent('alg:mgerakis_pgomes94_raph737/pysparkMapReduceJob.py', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
 
-		hospital_locations_resource = doc.entity('bdp:u6fv-m8v4', {'prov:label':'Hospital Locations', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-		traffic_locations_resource = doc.entity('bdp:dih6-az4h',{'prov:label':'Traffic Locations',prov.model.PROV_TYPE:'ont:DataResource','ont:Extension':'json'})
-		crime_locations_resource = doc.entity('bdp:29yf-ye7n',{'prov:label':'Crime Locations',prov.model.PROV_TYPE:'ont:DataResource','ont:Extension':'json'})
-		mbta_stops_resource = doc.entity('dat:mgerakis_pgomes94_raph737#stops',{'prov:label':'MBTA Stops',prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'csv'})
-		police_stations_resource = doc.entity('bdp:pyxn-r3i2',{'prov:label':'Police Locations',prov.model.PROV_TYPE:'ont:DataResource','ont:Extension':'json'})
+		hospital_scores_resource = doc.entity('bdp:u6fv-m8v4', {'prov:label':'Hospital Scores', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
 
-		get_hospital_locations = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-		get_traffic_locations  = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-		get_crime_locations    = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-		get_mbta_stops         = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-		get_police_stations    = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+		get_hospital_scores = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 
-		doc.wasAssociatedWith(get_hospital_locations, this_script)
-		doc.wasAssociatedWith(get_traffic_locations, this_script)
-		doc.wasAssociatedWith(get_crime_locations, this_script)
-		doc.wasAssociatedWith(get_mbta_stops, this_script)
-		doc.wasAssociatedWith(get_police_stations, this_script)
+		doc.wasAssociatedWith(get_hospital_scores, this_script)
 
-		doc.usage(get_hospital_locations,hospital_locations_resource,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
-		doc.usage(get_traffic_locations,traffic_locations_resource,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
-		doc.usage(get_crime_locations,crime_locations_resource,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
-		doc.usage(get_mbta_stops,mbta_stops_resource,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
-		doc.usage(get_police_stations,police_stations_resource,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
+		doc.usage(get_hospital_scores,hospital_scores_resource,startTime,None,{prov.model.PROV_TYPE:'ont:Retrieval'})
 
-		hospitals = doc.entity('dat:mgerakis_pgomes94_raph737#hospital_locations', {prov.model.PROV_LABEL:'Hospital Locations', prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(hospitals, this_script)
-		doc.wasGeneratedBy(hospitals, get_hospital_locations, endTime)
-		doc.wasDerivedFrom(hospitals, hospital_locations_resource)
-
-		traffic = doc.entity('dat:mgerakis_pgomes94_raph737#traffic_locations', {prov.model.PROV_LABEL:'Traffic Locations', prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(traffic, this_script)
-		doc.wasGeneratedBy(traffic, get_traffic_locations, endTime)
-		doc.wasDerivedFrom(traffic, traffic_locations_resource)
-
-		crimes = doc.entity('dat:mgerakis_pgomes94_raph737#crime_locations', {prov.model.PROV_LABEL:'Crime Locations', prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(crimes, this_script)
-		doc.wasGeneratedBy(crimes, get_crime_locations, endTime)
-		doc.wasDerivedFrom(crimes, crime_locations_resource)
-
-		police_stations = doc.entity('dat:mgerakis_pgomes94_raph737#police_station_locations', {prov.model.PROV_LABEL:'Police Station Locations', prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(police_stations, this_script)
-		doc.wasGeneratedBy(police_stations, get_police_stations, endTime)
-		doc.wasDerivedFrom(police_stations, police_stations_resource)
-
-		mbta_stops = doc.entity('dat:mgerakis_pgomes94_raph737#mbta_stop_locations', {prov.model.PROV_LABEL:'MBTA Stop Locations', prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(mbta_stops, this_script)
-		doc.wasGeneratedBy(mbta_stops, get_mbta_stops, endTime)
-		doc.wasDerivedFrom(mbta_stops, mbta_stops_resource)
+		hospitalScores = doc.entity('dat:mgerakis_pgomes94_raph737#hospital_scores', {prov.model.PROV_LABEL:'Hospital Scores', prov.model.PROV_TYPE:'ont:DataSet'})
+		doc.wasAttributedTo(hospitalScores, this_script)
+		doc.wasGeneratedBy(hospitalScores, get_hospital_scores, endTime)
+		doc.wasDerivedFrom(hospitalScores, hospital_scores_resource)
 
 		repo.record(doc.serialize())
 		repo.logout()
@@ -148,8 +112,8 @@ class getError(dml.Algorithm):
 		return doc
 
 getError.execute(trial=True)
-#doc = dataRequests.provenance()
-#print(doc.get_provn())
-#print(json.dumps(json.loads(doc.serialize()), indent=4))
+doc = dataRequests.provenance()
+print(doc.get_provn())
+print(json.dumps(json.loads(doc.serialize()), indent=4))
 
 ##eof
