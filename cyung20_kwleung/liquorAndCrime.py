d0dd8829dfd7fe087560609ca1de04a114347470
kevinwleung <kwleung@bu.edu>
2016-11-15 18:51:42 -0500

    Update liquorAndCrime.py

--- cyung20_kwleung/liquorAndCrime.py
+++ cyung20_kwleung/liquorAndCrime.py
@@ -11,7 +11,7 @@ class liquorAndCrime(dml.Algorithm):
     writes = ['cyung20_kwleung.liquorAndCrime']
 
     @staticmethod
-    def execute(trial = False):
+    def execute(trial = True):
         client = dml.pymongo.MongoClient()
         repo = client.repo 
         repo.authenticate("cyung20_kwleung", "cyung20_kwleung") 
@@ -44,8 +44,12 @@ class liquorAndCrime(dml.Algorithm):
                     offense = theData['offense_code_group']
                 except KeyError:
                     offense = None
+                try: 
+                    day = theData['day_of_week']
+                except KeyError:
+                    day = None
                 if lon != None and lat != None:
-                    crimeDetails += [['crime', [lon,lat],offense]]
+                    crimeDetails += [['crime', [lon,lat],offense,day]]
             counter += 1
         
         data = repo.cyung20_kwleung.liquor.find() 
@@ -80,23 +84,72 @@ class liquorAndCrime(dml.Algorithm):
             #If the distance is less or equal to 100 meters, keep it. 
             return dist <= 100
         
-        #Number of alcohol places at least 25 meters from crime.
-        #print(len(select(product(liquorDetails,crimeDetails),dis)))
-        
-        #Alcohol places at least 25 meters from crime.
+        #Alcohol places at least 100 meters from crime.
         selected = select(product(liquorDetails,crimeDetails),dis)
         
         instances = {}
         #for x in range(0, len(selected)):
         num = 1
         for x in range(0, len(selected)):
+            #liquor name
             one = str(selected[x][0][1])
+            #liquor location
             two = str(selected[x][0][2])
+            #crime location
             three = str(selected[x][1][1])
+            #crime offense
             four = str(selected[x][1][2])
-            if four == "Harassment" or four == "Aggravated Assault" or four == "Simple Assault":
-                repo.cyung20_kwleung.liquorAndCrime.insert_one({'liquor_name':one, 'liquor_location':two, 'crime_location':three, 'crime_offense':four})
+            #day (mon-friday)
+            five = str(selected[x][1][3])
+            
+            # If these crimes, add it to the liquorAndCrime repo
+            if four == "Harassment" or four == "Aggravated Assault"\
+            or four == "Simple Assault" or four == "Sex Offender Registration"\
+            or four == "Homicide":
+                repo.cyung20_kwleung.\
+                liquorAndCrime.insert_one({'liquor_name':one, 'liquor_location'\
+                :two, 'crime_location':three, 'crime_offense':four, 'day':five})
                 num += 1
+        
+        data = repo.cyung20_kwleung.liquorAndCrime.find()
+        
+        # Mon. to Thurs. Selected Crimes
+        monToThurs = []
+        
+        count = 0
+        for d in data:
+            count += 1
+            if dict(d)['day'] == 'Monday' or dict(d)['day'] == 'Tuesday' or dict(d)['day']\
+             == 'Wednesday' or dict(d)['day'] == 'Thursday':
+                monToThurs.append(dict(d))
+                
+        # Avg selected crimes per day from Monday to Thursday
+        monThursAvg = len(monToThurs)/4
+
+        print("Selected Crimes Total Within 100 meters of Liquor Store: " + str(count))
+        print()
+        print("Selected Crimes Total Within 100 meters of Liquor Store (Mon-Thurs): "\
+              + str(len(monToThurs)))
+        print("Selected Crimes Avg Within 100 meters of Liquor Store (Mon-Thurs): "\
+              + str(monThursAvg))
+        print()
+        
+        data = repo.cyung20_kwleung.liquorAndCrime.find()
+        
+        # Friday to Sunday Selected Crimes
+        frToS = []
+        for d in data:
+            if dict(d)['day'] == 'Friday' or dict(d)['day'] == 'Saturday'\
+             or dict(d)['day'] == 'Sunday':
+                frToS.append(dict(d))
+                
+        # Avg selected crimes per day from Friday to Sunday
+        frToSAvg = len(frToS)/3
+
+        print("Selected Crimes Total Within 100 meters of Liquor Store (Fri-Sun): "\
+              + str(len(frToS)))
+        print("Selected Crimes Avg Within 100 meters of Liquor Store (Fri-Sun): "\
+              + str(frToSAvg))
         
         repo.logout()
 
@@ -126,13 +179,14 @@ class liquorAndCrime(dml.Algorithm):
         doc.used(this_final, liquorLicense, startTime)
         doc.used(this_final, crimeReports, startTime)
         
-        crimeLiq = doc.entity('dat:liquorAndCrime', {prov.model.PROV_LABEL:'Proximity of crime locations and liquor', prov.model.PROV_TYPE:'ont:DataSet'})
+        crimeLiq = doc.entity('dat:liquorAndCrime', {prov.model.PROV_LABEL:'Selected Crimes Within 100 Meter of Liquor Stores', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(crimeLiq, this_script)
         doc.wasGeneratedBy(crimeLiq, this_final, endTime)
         doc.wasDerivedFrom(crimeLiq, crimeReports, this_final, this_final, this_final)
         doc.wasDerivedFrom(crimeLiq, liquorLicense, this_final, this_final, this_final)
         
         repo.record(doc.serialize())
+    
         repo.logout()
         
         return doc
@@ -145,9 +199,3 @@ print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))
 
 #Print out new data set!
-data = repo.cyung20_kwleung.liquorAndCrime.find()
-
-'''count = 0
-for d in data:
-    count += 1 
-print(count)'''
