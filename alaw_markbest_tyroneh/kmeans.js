2eeff21e34fc9212c29ecd1704f470dfd00864cd
Tyrone Hou <tyronehou@dhcp-wifi-8021x-155-41-48-253.bu.edu>
2016-11-14 23:12:50 -0500

    Implement working kmeans & projection of points for bus stop 99800

--- alaw_markbest_tyroneh/kmeans.js
+++ alaw_markbest_tyroneh/kmeans.js
@@ -1,5 +1,8 @@
 /*
+This javascript file performs a kmeans on a set of weighted points in order to determine
+clustered regions of interest. The kmeans is then reprojected onto the route to determine an optimal allocation of bus stops that covers the most regions.
 We define an algorithm in order to perform kmeans on a set of weighted points
+
 Initial means will be bus stops with a list of routes they could be on
 (stop_id, coordinates, route_list)
 
@@ -19,43 +22,48 @@ Modified kmeans
     for route in routes:
         for variant in route:
             find closest projection to mean
+
+As proof of concept we test our algorithm on Route 99800
 */
 
 function _dist(u, v) {
     return Math.pow(u[0] - v[0], 2) + Math.pow(u[1] - v[1], 2);
 }
 
-function dot(u, v) {
+function _dot(u, v) {
     return u[0]*v[0] + u[1]*v[1];
 }
 
-function scale(v, c) {
+function _scale(v, c) {
     return [c*v[0], c*v[1]];
 }
 
-function add(u, v) {
+function _add(u, v) {
     return [u[0] + v[0], u[1] + v[1]];
 }
 
-db.system.js.save({ _id:"dist", value:_dist});
-
 // Finds the minimum projection 
-db.system.js.save({_id:"findnorm", value:function(p, pline) {
+function _findnorm(p, pline) {
     var min_norm = 0;
     for(var i = 1; i < pline.length; i++) {
         var q0 = pline[i-1]; var q1 = pline[i];
         q = [q1[0]-q0[0], q1[1]-q0[1]];
-        var proj = scale(dot(p, q) / _dist(q, [0,0]), q);
+
+        var c = dot(p, q) / dist(q, [0,0]);
+        var proj = scale(q, c);
         var norm = add(proj, scale(p, -1));
         if(i == 1) {
             min_norm = norm;
         }
         else {
-            min_norm = Math.min(norm, min_norm);
+            if(dist(norm, [0,0]) < dist(min_norm, [0,0])) {
+                min_norm = norm;
+            }
         }
     }
     return min_norm;
-}});
+}
+
 
 db.loadServerScripts();
 
@@ -83,36 +91,24 @@ var R = "alaw_markbest_tyroneh.R";
 var MP = "alaw_markbest_tyroneh.MP";
 var MPD = "alaw_markbest_tyroneh.MPD";
 var MPD2 = "alaw_markbest_tyroneh.MPD2";
-var M2 = "alaw_markbest_tyroneh.M2";
-var M2R = "alaw_markbest_tyroneh.M2R";
+var MR = "alaw_markbest_tyroneh.MR";
 
 // Initialize the database of means
 
 dropPerm(M);
 createPerm(M);
 
-db.alaw_markbest_tyroneh.BusStops.mapReduce(
-    function() {
-        var props = this.properties;
-        var coordinates = this.geometry.coordinates;
-        var lat = coordinates[0]; var lon = coordinates[1];
-        if((lat >= 42.23) && (lat <= 42.41) && (lon >= -71.18) && (lon <= -70.993)) {
+var route_id = 99800
+var route = db.alaw_markbest_tyroneh.BusRoutes.find({"properties.ctps_id":route_id}).map(function(u) {return u})[0];
 
-        for(var i = 0; i < props.route_list.length; i++) {
-        if(props.route_list[i] == 34400) {
-        emit(props.stop_id, {
-                "coordinates": coordinates,
-                "route_list": props.route_list
-            });
-        break;
-        }
-        }
-        }
-    },
-    function(k, vs) {},
-    {out: M}
-);
-flatten(M);
+var means = [];
+
+for(var i = 0; i < route.properties.route_stops.length; i++) {
+    var lat = Math.random() * (42.41-42.23) + 42.23;
+    var lon = Math.random() * (71.18 - 70.993) - 71.18;
+    means.push({"coordinates":[lat, lon]});
+}
+db[M].insert(means);
 
 dropPerm(P);
 createPerm(P);
@@ -158,7 +154,7 @@ db.alaw_markbest_tyroneh.StationsGeoJSONs.mapReduce(
         var weight;
         var coords = this.value.geometry.coordinates;
         var lat = coords[0]; var lon = coords[1];
-        if((lat >= 42.3) && (lat <= 42.41) && (lon >= -71.10) && (lon <= -70.993)) {
+        if((lat >= 42.23) && (lat <= 42.41) && (lon >= -71.18) && (lon <= -70.993)) {
             var willEmit = true; 
             if(type == "Hubway") {
                 weight = this.value.properties.capacity;
@@ -184,14 +180,17 @@ db.alaw_markbest_tyroneh.StationsGeoJSONs.mapReduce(
 );
 flatten(P);
 
+
 dropPerm(R);
 createPerm(R);
 
 db.alaw_markbest_tyroneh.BusRoutes.mapReduce(
     function() {
-        emit(this.properties.ctps_id, {
-            "paths": [this.geometry.coordinates]
-        });
+        if(this.properties.ctps_id == rid) {
+            emit(this.properties.ctps_id, {
+                "paths": [this.geometry.coordinates]
+            });
+        }
     },
     function(k, vs) {
         var paths = [];
@@ -209,7 +208,8 @@ db.alaw_markbest_tyroneh.BusRoutes.mapReduce(
         });
         return { "paths": paths }
     },
-    {out: R}
+    { scope: {rid:route_id},
+      out: R}
 );
 
 flatten(R);
@@ -237,6 +237,7 @@ do {
     prod(M, P, MP);
     dropPerm(MPD);
     createPerm(MPD);
+
     db.alaw_markbest_tyroneh.MP.mapReduce(
         function() {
             emit(this._id, {
@@ -254,6 +255,7 @@ do {
     flatten(MPD);
     dropPerm(MPD2);
     createPerm(MPD2);
+
     db.alaw_markbest_tyroneh.MPD.mapReduce(
         function() { emit(this.p._id, this); },
         function(k, vs) {
@@ -266,8 +268,7 @@ do {
         },
         {out: MPD2}
     );
-    dropPerm(M2);
-    createPerm(M2);
+
     db.alaw_markbest_tyroneh.MPD2.mapReduce(
         function() {
             var coords = this.value.p.coordinates;
@@ -275,60 +276,85 @@ do {
             var weighted_coords = [coords[0]*weight, coords[1]*weight];
             emit(this.value.m._id, {
                 "coordinates": weighted_coords,
-                "route_list": this.value.m.route_list,
                 "c": weight
             })
         },
         function(k, vs) {
-            print("key", k);
             var lat = 0, lon = 0, c= 0;
             vs.forEach(function(v) {
                 lat += v.coordinates[0];
                 lon += v.coordinates[1];
-                c += v.weight;
+                c += v.c;
             });
-            return {"coordinates":[lat, lon], "route_list": vs[0].route_list, "c":c};
+            return {"coordinates":[lat, lon], "c":c};
         },
-        { finalize: function(k, v) { return {"_id": k, "coordinates": [v.coordinates[0]/v.c, v.coordinates[1]/v.c], "route_list":v.route_list}; },
-          out: M2
+        { finalize: function(k, v) { return {"_id": k, "coordinates": [v.coordinates[0]/v.c, v.coordinates[1]/v.c]}; },
+          out: M
         }
     );
-/*
-    prod(M2, R, M2R);
-    db.alaw_markbest_tyroneh.M2R.mapReduce(
-        function() {
-            var m = this.left
-            var r = this.right
-            for(var i = 0; i < m.route_list.length; i++) {
-                var route = m.route_list[i]
-                if(r._id == route) {
-                    var min_norm_for_route = findnorm(m.coordinates, route);
-                    emit(m.stop_id, {"min_norm": min_norm_for_route, "route_list":m.route_list});
-                    break;
-                }
-            }
-        },
-        function(k, vs) {
-            var min_norm = vs[0].min_norm;
-            vs.forEach(function(stop) {
-                min_norm = min(stop.min_norm, min_norm);
-            });
-            return {"min_norm": min_norm, "route_list":vs[0].route_list};
-        },
-        { finalize: function(k, v) {
-                return {
-                    "_id":k,
-                    "coordinates":add(v.coordinates, v.min_norm),
-                    "route_list":v.route_list};
-            },
-          out: M }
-    );
-   */ 
-    var o = 0;
-    var n = 0;
-    print('Ummmmmm');
-}
-while(o != n);
 
+    flatten(M);
+    db.alaw_markbest_tyroneh.M.mapReduce(
+        function() { emit("hash", {hash: this.coordinates[0] + this.coordinates[1]}); },
+        function(k, vs) {
+            var hash = 0;
+            vs.forEach(function(v) {
+                hash += v.hash;
+            });
+            return {"hash": hash};
+        },
+        {out: "alaw_markbest_tyroneh.HASHNEW"}
+    );
+    var o = db.alaw_markbest_tyroneh.HASHOLD.find({}).limit(1).toArray()[0].value.hash;
+    var n = db.alaw_markbest_tyroneh.HASHNEW.find({}).limit(1).toArray()[0].value.hash;
+    print(o);
+    print(n);
+    print(iter);
+    iter++;
+}
+while(o != n && iter < 20);
+
+prod(M, R, MR);
+dropPerm("alaw_markbest_tyroneh.NewStops");
+createPerm("alaw_markbest_tyroneh.NewStops");
+
+db.alaw_markbest_tyroneh.MR.mapReduce(
+    function() {
+        var m = this.left
+        var route = this.right
+        if(route._id == rid) {
+            route.paths.forEach(function(path) {
+                    var min_norm_for_route = findnorm(m.coordinates, path);
+                    emit(m._id, {
+                        "min_norm": min_norm_for_route,
+                        "coordinates":m.coordinates
+                    });
+            });
+        }
+    },
+    function(k, vs) {
+        var min_norm = vs[0].min_norm;
+        vs.forEach(function(stop) {
+            min_norm = Math.min(stop.min_norm, min_norm);
+        });
+        return {"min_norm": min_norm, "coordinates":vs[0].coordinates};
+    },
+    { scope: {rid:route_id, add:_add, findnorm:_findnorm, scale:_scale, dot:_dot, dist:_dist},
+      finalize: function(k, v) {
+            return {
+                "_id":k,
+                "coordinates":add(v.coordinates, v.min_norm),
+                "norm":v.min_norm
+            };},
+      out: "alaw_markbest_tyroneh.NewStops" }
+);
+
+flatten("alaw_markbest_tyroneh.NewStops");
+
+//Drop all intermediate datasets
+var intermediate = [M, P, R, MP, MPD, MPD2, MR]
 dropPerm("alaw_markbest_tyroneh.HASHOLD");
-dropPerm("alaw_markbest_tyroneh.HASHOLD");
+dropPerm("alaw_markbest_tyroneh.HASHNEW");
+intermediate.forEach(function(v) {
+    dropPerm(v);
+});
