83adc5cfcf636c0713572cbe904e4d0815eeb90d
Ying Hang Eng <eyinghang@gmail.com>
2016-12-04 14:50:42 -0500

    commiting missed commits

--- alsk_yinghang/correlation_lights_crimes.py
+++ alsk_yinghang/correlation_lights_crimes.py
@@ -13,7 +13,7 @@ class correlation_lights_crimes(dml.Algorithm):
     writes = ['alsk_yinghang.lights_crimes']
 
     @staticmethod
-    def execute(trial=False):
+    def execute(trial):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('alsk_yinghang', 'alsk_yinghang')
