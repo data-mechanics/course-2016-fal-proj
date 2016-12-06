ddbb3b6b1cc58f9fe075716733b3e916443189cc
Raphib737 <raph737@bu.edu>
2016-12-06 00:22:47 -0500

    Removed node modules and added more RESTFUL urls

--- mgerakis_pgomes94_raph737/webapp/index.js
+++ mgerakis_pgomes94_raph737/webapp/index.js
@@ -17,8 +17,14 @@ var mongodb = require('mongodb')
 
 app.use(express.static(__dirname + '/'));
 
-app.get('/test', function (req, res) {
-	res.send(getHospitalData(mongodb))
+app.get('/hospitals', function (req, res) {
+	const cb = docs => res.send({'hospitals': docs});
+	getCollectionData(cb,"mgerakis_pgomes94_raph737.hospital_locations");
+});
+
+app.get('/crimes', function(req,res){
+	const cb = docs => res.send({'crimes': docs});
+	getCollectionData(cb,"mgerakis_pgomes94_raph737.crime_locations")
 })
 
 app.listen(3000, function () {
@@ -26,20 +32,19 @@ app.listen(3000, function () {
 })
 
 
-function getHospitalData(mongodb){
+function getCollectionData(cb,collectionName){
 	var MongoClient = mongodb.MongoClient;
-
 	var url = "mongodb://localhost:27017/repo"
 
 	MongoClient.connect(url,function (err, db) {
 	  if (err) {
-	    console.log('Unable to connect to the mongoDB server. Error:', err);
+	  	console.log('Unable to connect to the mongoDB server. Error:', err);
+	  	cb(err);
 	  }else {
 	    console.log('Connection established to', url);
-	    var collection = db.collection("mgerakis_pgomes94_raph737_hospital_locations");
-	    console.log(collection);
-	    collection.find({}).toArray(function(err,hospital){
-	    	console.log(hospital)
+	    var collection = db.collection(collectionName);
+	    collection.find({}).toArray((err, docs) => {
+	    	cb(docs);
 	    });
 	  }});
-}
+	}
