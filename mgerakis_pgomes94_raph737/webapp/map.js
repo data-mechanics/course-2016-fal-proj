6f265ec4be5ab0fb64d677e8ea6272c044e404aa
Raphib737 <raph737@bu.edu>
2016-12-06 10:14:26 -0500

    Got all the markers on the map

--- mgerakis_pgomes94_raph737/webapp/map.js
+++ mgerakis_pgomes94_raph737/webapp/map.js
@@ -7,26 +7,69 @@ var labelIndex = 0;
 
 function initialize() {
 	url = "http://localhost:3000/api/hospitals"
-	data = fetch(url).then(res => res.json());
-	console.log(data)
-	var boston = { lat: 42.3601, lng: -71.0589 };
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 12,
-	  center: boston
-	});
+	fetch(url).then(res => res.json()).then(function(hospitalData){
+		var boston = { lat: 42.3601, lng: -71.0589 };
+		var map = new google.maps.Map(document.getElementById('map'), {
+  		zoom: 11,
+  		center: boston
+		});
+		addHospitalMarkers(hospitalData.hospitals,map)
+		url = "http://localhost:3000/api/crimes"
+		fetch(url).then(res => res.json()).then(function(crimeData){
+			addCrimeMarkers(crimeData.crimes,map)
+			url = "http://localhost:3000/api/mbtastops"
+			fetch(url).then(res => res.json()).then(function(mbtaStops){
+				// addMbtaMarkers(mbtaStops.mbta_stops,map);
+				url = "http://localhost:3000/api/policestations"
+				fetch(url).then(res => res.json()).then(function(policestations){
+					addPoliceStationMarkers(policestations.policestations,map)
+					url = "http://localhost:3000/api/trafficlocs"
+					fetch(url).then(res => res.json()).then(function(trafficlocs){
+						addtrafficlocsMarkers(trafficlocs.trafficlocs,map)
+						url = "http://localhost:3000/api/optimalcoords"
+						fetch(url).then(res => res.json()).then(function(optimalcoords){
+							addOptimalCoordsMarker(optimalcoords.optimalcoord,map)
+						})
+					})
+				})
+			})
+		})
+	})}
 
-	// This event listener calls addMarker() when the map is clicked.
-	google.maps.event.addListener(map, 'click', function(event) {
-	  addMarker("Hospital",event.latLng, map);
-	});
+function addOptimalCoordsMarker(optimalcoord,map){
+	addMarker('Optimal Coord',{lat:parseFloat(optimalcoord[0].optimal_coord[0]),lng:parseFloat(optimalcoord[0].optimal_coord[1])},map,'blue')
+}
+function addtrafficlocsMarkers(trafficlocs,map){
+	for(var i = 0; i < trafficlocs.length; i++){
+		addMarker('',{lat:parseFloat(trafficlocs[i].location[0]),lng:parseFloat(trafficlocs[i].location[1])},map,'red')
+	}
+}
 
-	// Add a marker at the center of the map.
-	addMarker("Boston", boston, map);
+function addPoliceStationMarkers(policestations,map){
+	for(var i = 0; i < policestations.length; i++){
+		addMarker(policestations[i].identifier,{lat:parseFloat(policestations[i].location[0]),lng:parseFloat(policestations[i].location[1])},map,'green')
+	}
+}
+function addMbtaMarkers(mbtaStops,map){
+	for(var i = 0; i < mbtaStops.length; i++){
+		addMarker('',{lat:parseFloat(mbtaStops[i].location[0]),lng:parseFloat(mbtaStops[i].location[1])},map,'green')
+	}
+}
+function addCrimeMarkers(crimeData,map){
+	for(var i = 0; i < crimeData.length; i++){
+		addMarker('',{lat:parseFloat(crimeData[i].location[0]),lng:parseFloat(crimeData[i].location[1])},map,'red')
+	}
+}
+
+function addHospitalMarkers(hospitalData,map){
+	for(var i = 0; i < hospitalData.length; i++){
+		addMarker(hospitalData[i].identifier,{lat:hospitalData[i].location[0],lng:hospitalData[i].location[1]}, map,'green');	
+	}
 }
 
 
 // Adds a marker to the map.
-function addMarker(locationName,location, map) {
+function addMarker(locationName,location, map,color) {
 	// Add the marker at the clicked location, and add the next-available label
 	// from the array of alphabetical characters.
 	var marker = new google.maps.Marker({
@@ -34,7 +77,12 @@ function addMarker(locationName,location, map) {
 	  label: locationName,
 	  map: map
 	});
-	marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
+	marker.setIcon("http://labs.google.com/ridefinder/images/mm_20_".concat(color,'.png'))
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+	// // This event listener calls addMarker() when the map is clicked.
+	// google.maps.event.addListener(map, 'click', function(event) {
+	//   addMarker("Hospital",event.latLng, map);
+	// });
