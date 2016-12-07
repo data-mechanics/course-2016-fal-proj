9ecfb122b52e74386cc9d9f750152be2f450681d
Nisa Gurung <nisagurung@dhcp-wifi-8021x-168-122-10-92.bu.edu>
2016-12-07 18:34:57 -0500

    legend and descriptions

--- ktan_ngurung_yazhang_emilyh23/static/index.js
+++ ktan_ngurung_yazhang_emilyh23/static/index.js
@@ -16,14 +16,10 @@ function initMap() {
   //       .openPopup();
 
   function getColor(d) {
-    return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
+    return d > 200  ? '#E31A1C' :
            d > 100  ? '#FC4E2A' :
            d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
+                        '#FFEDA0';
   }
 
   function style(feature) {
@@ -37,6 +33,31 @@ function initMap() {
       };
   }
   L.geoJson(zcdata, {style: style}).addTo(map);
+
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [50, 100, 200],
+      labels = [],
+      from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+      from = grades[i] + 1;
+      to = grades[i + 1];
+      rating = i + 1 
+
+      labels.push(
+        '<i style="background:' + getColor(from) + '"></i> ' +
+        rating);
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+  };
+
+  legend.addTo(map);
 }
 
 function getResult() {
