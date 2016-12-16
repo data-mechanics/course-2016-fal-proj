dac15cbaf853c211e8a878e83c55a7a68cce57d3
Jennifer Tsui <jgtsui@bu.edu>
2016-12-15 21:17:00 -0500

    updated visualization 2 (scatter plot)

--- aliyevaa_bsowens_dwangus_jgtsui/correlation.py
+++ aliyevaa_bsowens_dwangus_jgtsui/correlation.py
@@ -160,6 +160,35 @@ class correlation(dml.Algorithm):
         createTxtFiles(crimeHeatmapValues, 'crimeHeatmapValues.txt')
         createTxtFiles(propertValHeatmapValues, 'propertyHeatmapValues.txt')
         createTxtFiles(communityValCellKeys, 'communityHeatmapValues.txt')
+
+        '''
+        Written by Jennifer Tsui
+        12/15/16
+
+        Generate CSV files from heatmap text files.
+        Eventually assemble the csv we need for the scatter plot visualization by 
+        1) adding a title row (in the form of x1, y1, z1, ... , x3, y3, z3)
+        2) making each x1, y1, z1 correspond with latitude, longitude, and value
+           for community scores, crime scores, and property scores.
+
+        Note: did that using Microsoft Excel. File can be found in the 'Data' folder
+              as CommunityPropertyCrimeScatter.CSV
+        '''
+        with open('text_and_csv/communityHeatmapValues.txt') as fin, open('communityScatter.csv', 'w') as fout:
+            o=csv.writer(fout)
+            for line in fin:
+                o.writerow(line.split())
+
+        with open('text_and_csv/crimeHeatmapValues.txt') as fin, open('crimeScatter.csv', 'w') as fout:
+            o=csv.writer(fout)
+            for line in fin:
+                o.writerow(line.split())
+
+        with open('text_and_csv/propertyHeatMapValues.txt') as fin, open('propertyScatter.csv', 'w') as fout:
+            o=csv.writer(fout)
+            for line in fin:
+                o.writerow(line.split())    
+
         return
     
     @staticmethod
