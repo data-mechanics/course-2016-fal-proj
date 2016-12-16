9945f8d8f945e9a9ea19f826f049b0135a553d85
Tyrone Hou <tyroneh@bu.edu>
2016-12-15 22:40:41 -0500

    Add kmeans visualization to webservice

--- alaw_markbest_tyroneh/visualizations/server.py
+++ alaw_markbest_tyroneh/visualizations/server.py
@@ -22,12 +22,22 @@ def getAllocationScores():
 	allocationScores = json.load(open('../jsons/allocationScores.json','r'))
 	return jsonify(allocationScores)
 
+@app.route('/routes')
+def getRoutes():
+	routes = json.load(open('../jsons/routesGeoJSONs.json', 'r'))
+	return jsonify(routes)
 
 @app.route('/allocation', methods=['GET'])
 def loadAllocation():
 	'''loads the visualization for bus allocation'''
 
 	return render_template('allocation.html')
+
+@app.route('/kmeans', methods=['GET'])
+def loadKMeans():
+	'''loads the visualization for bus allocation'''
+
+	return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
